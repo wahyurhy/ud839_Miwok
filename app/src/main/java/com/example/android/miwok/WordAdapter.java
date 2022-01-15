f7f9a7504374de17167ca5e98ee7134a80ffc440
wahyurhy <wahyurahayu82@gmail.com>
2022-01-16 06:14:58 +0700

    adding list + adapter into numberActivity

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,41 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    private static final String LOG_TAG = WordAdapter.class.getSimpleName();
+
+    public WordAdapter(Activity context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView miwokWord = (TextView) listItemView.findViewById(R.id.miwok_word);
+        miwokWord.setText(currentWord.getmMiwokTranslation());
+
+        TextView defaultTranslation = (TextView) listItemView.findViewById(R.id.default_translation);
+        defaultTranslation.setText(currentWord.getmDefaultTranslation());
+
+        return listItemView;
+    }
+}
